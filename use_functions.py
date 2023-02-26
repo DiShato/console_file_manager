29003a84734b843719a796030000c020fde270e4
DiShato <krevetka082@gmail.com>
2023-02-27 01:09:19 +0700

    editing files : exception handling

--- use_functions.py
+++ use_functions.py
@@ -36,37 +36,51 @@
 import os
 import json
 
-def choice_1(cash, cash_deposit = 0):
+
+def choice_1(cash, cash_deposit=0):
     '''
     :param cash_deposit: сумма пополнения
     :return: сумма с учетом пополнения
     '''
-    cash_new = cash + cash_deposit
-    f = open('cash.txt', 'a')
-    f.write(str(cash_deposit)+',')
-    f.close()
-    return cash_new
+    try:
+        cash = float(cash)
+        cash_deposit = float(cash_deposit)
+    except:
+        print('Ошибка! Введено не число! Повторите попытку!')
+    else:
+        cash_new = cash + cash_deposit
+        f = open('cash.txt', 'a')
+        f.write(str(cash_deposit) + ',')
+        f.close()
+        return cash_new
 
-def choice_2(cash, sum_shop = 0, name_shop = 'покупка'):
+
+def choice_2(cash, sum_shop=0, name_shop='покупка'):
     '''
     :param cash: сумма на счете
     :param sum_shop: сумма покупки
     :param name_shop: наименование покупки
     :return:
     '''
-    if cash >= sum_shop:
-        cash -= sum_shop
-    elif cash < sum_shop:
-        print('денег не хватает')
+    try:
+        cash = float(cash)
+        sum_shop = float(sum_shop)
+    except:
+        print('Ошибка! Введено не число! Повторите попытку!')
+    else:
+        if cash >= sum_shop:
+            cash -= sum_shop
+        elif cash < sum_shop:
+            print('денег не хватает!')
 
-    f = open('history_shop.txt', 'a')
-    f.write('покупка {} : {}руб, '.format(name_shop,sum_shop))
-    f.close()
+        f = open('history_shop.txt', 'a')
+        f.write('покупка {} : {}руб, '.format(name_shop, sum_shop))
+        f.close()
 
     return cash
 
-def shoping():
 
+def shoping():
     # если нет файлов с историейпокупок и остатка, то создаем
     if 'cash.txt' not in os.listdir():
         os.path.exists('cash.txt')
@@ -84,19 +98,18 @@ def shoping():
 
         choice = input('Выберите пункт меню')
         if choice == '1':
-            cash =  choice_1(cash = int(list(open('cash.txt', 'r'))[0].split(',')[-2]),
-                             cash_deposit = int(input('введите сумму пополнения')))
+            cash = choice_1(cash=list(open('cash.txt', 'r'))[0].split(',')[-2],
+                            cash_deposit=input('введите сумму пополнения'))
 
         elif choice == '2':
-            cash =  choice_2(cash = int(list(open('cash.txt', 'r'))[0].split(',')[-2]),
-                                           sum_shop = int(input('Введите сумму покупки')),
-                                           name_shop = int(input('Введите наименование покупки')))
+            cash = choice_2(cash=list(open('cash.txt', 'r'))[0].split(',')[-2],
+                            sum_shop=input('Введите сумму покупки'),
+                            name_shop=input('Введите наименование покупки'))
 
         elif choice == '3':
-            print( list(open('history_shop.txt', 'r')))
+            print(list(open('history_shop.txt', 'r')))
 
         elif choice == '4':
             break
         else:
             print('Неверный пункт меню')
-# shoping()
--- victory.py
+++ victory.py
@@ -1,65 +1,61 @@
 from datetime import datetime
 import random
 
-def  play_victory():
-    count_random_writers = 5
+dictionary_of_writers = {'А.С. Пушкин': '06.06.1799',
+                         'М.Ю. Лермонтов': '15.10.1814',
+                         'А.П. Чехов': '15.06.1904',
+                         'Л.Н. Толстой': '09.09.1828',
+                         'М.А. Булгаков': '15.05.1940',
+                         'Н.В, Гоголь': '20.03.1852',
+                         'А.А. Блок': '23.11.1880',
+                         'И.С. Тургенев': '09.11.1818',
+                         'И.А. Бунин': '22.10.1870',
+                         'В.П. Некрасов': '17.06.1911'}
+
+
+def play_victory(count_random_question=5, new_game='yes', dict_of_victory=dictionary_of_writers):
+    '''
+
+    :param count_random_question:  кол-во вопросов для викторины
+    :param new_game: параметр запуска игры
+    :param dict_of_victory: список вопросов и ответов для викторины
+    :return:
+    '''
+
     right_answer = 0
     wrong_answer = 0
-    new_game = 'yes'
-
-    dictionary_of_writers = {'А.С. Пушкин': '06.06.1799',
-                             'М.Ю. Лермонтов': '15.10.1814',
-                             'А.П. Чехов': '15.06.1904',
-                             'Л.Н. Толстой': '09.09.1828',
-                             'М.А. Булгаков': '15.05.1940',
-                             'Н.В, Гоголь' : '20.03.1852',
-                             'А.А. Блок' : '23.11.1880',
-                             'И.С. Тургенев' : '09.11.1818',
-                             'И.А. Бунин' : '22.10.1870',
-                             'В.П. Некрасов' : '17.06.1911'
-                             }
-
-
 
     while new_game == 'yes':
 
-        dictionary_of_writers_new = {}
+        dict_of_victory_new = {}
+        answer = None
 
+        # отбираем (count_random_question несколько случайных писателей из словаря c помощью sample
+        random_question = random.sample(list(dict_of_victory.keys()), count_random_question)
 
+        # записываем в новый словарь отобранное кол-во пар вопрос-ответ
+        for i in range(count_random_question):
+            dict_of_victory_new[random_question[i]] = dict_of_victory.get(random_question[i])
 
-        # отбираем (count_random_writers) несколько случайных писателей из словаря
-        random_writers = random.sample(list(dictionary_of_writers.keys()), count_random_writers)
+        # проходим по вопросам словаря и проводим викторину
+        for i in range(len(list(dict_of_victory_new.keys()))):
 
-        for i in range(count_random_writers):
-            dictionary_of_writers_new[random_writers[i]] = dictionary_of_writers.get(random_writers[i])
+            dict_question = list(dict_of_victory_new.keys())[i]  # вопрос i
+            the_answer_to_the_question = list(dict_of_victory_new.values())[i]  # ответ на вопрос i
 
+            while answer != str(the_answer_to_the_question):
 
-
-        year_ = None
-
-
-
-        # проходим по случайному списку писателей и проводим викторину
-        for i in range(len(list(dictionary_of_writers_new.keys()))):
-
-            name_writers = list(dictionary_of_writers_new.keys())[i]
-            birthday_writers = list(dictionary_of_writers_new.values())[i]
-
-            while year_ != str(birthday_writers):
-
-                year_ = input('введите год рождения в формате dd.mm.yyyy {}: '.format(name_writers))
-
-                if year_ == str(birthday_writers):
-                    right_answer += 1
-
-                    #if year_ == list(dictionary_of_writers_new.values())[-1]:
-                     #   break
+                answer = input('введите год рождения в формате dd.mm.yyyy {}: '.format(dict_question))
+                try:
+                    print(datetime.strptime(answer, '%d.%m.%Y'))  # проверка корректности записи даты
+                except:
+                    print('Введите дату в соответствии с форматом dd.mm.yyyy')
                 else:
-                    wrong_answer += 1
-                    dt = datetime.strptime(birthday_writers, '%m.%d.%Y')
-                    print('правильный ответ :', dt.strftime('%B %d, %Y'))
-
-
+                    if answer == the_answer_to_the_question:
+                        right_answer += 1
+                    else:
+                        wrong_answer += 1
+                        print('правильный ответ :', the_answer_to_the_question)
 
         # результаты викторины
         print('количество правильных ответов : ', right_answer)
